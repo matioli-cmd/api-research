757b10c97fef88d7b4c6844ca38ee153aef378c4
Gabriel Matioli <gabrielmatioli@Gabriels-MacBook-Air.local>
2024-11-24 14:10:32 -0700

    fg

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,8 +8,8 @@ function Header(){
 
              <div className="Header-links">
                         <Link className="button-link" to={'/api-research/'}><h2>Home</h2></Link>
-                        <Link className="button-link" to={'/api-research/faq'}><h2>FAQ</h2></Link>
                         <Link className="button-link" to={'/api-research/favorites'}><h2>Favorites</h2></Link>
+                        <Link className="button-link" to={'/api-research/faq'}><h2>FAQ</h2></Link>
             </div>
         
         </header>
